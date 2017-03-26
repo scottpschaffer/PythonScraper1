36e60c7f269c4b2306549d4b00c5bdabb8d083bc
scottpschaffer <scottpschaffer@yahoo.com>
2017-03-26 18:37:31 -0500

    Made more progress with Django

--- mysite1/mysite1/urls.py
+++ mysite1/mysite1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mysite1.views import hello, current_datetime, hours_ahead
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 ]
